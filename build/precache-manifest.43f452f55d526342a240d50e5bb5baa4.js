a78617185a1900d6273c5a3fbcf2b246134d20c7
emreavcilar <emreavcilar@gmail.com>
2020-01-09 02:14:37 +0300

    build added

---
+++ build/precache-manifest.43f452f55d526342a240d50e5bb5baa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bb8b9fa92cbeac421b44158828f43f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "637a33e4bb9f35ca7820",
+    "url": "/static/css/main.44c91f92.chunk.css"
+  },
+  {
+    "revision": "fa93c94d93771d1bb8b5",
+    "url": "/static/js/2.9bc1d081.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.9bc1d081.chunk.js.LICENSE"
+  },
+  {
+    "revision": "637a33e4bb9f35ca7820",
+    "url": "/static/js/main.c1c76f2a.chunk.js"
+  },
+  {
+    "revision": "1caa2948e15dc08abb40",
+    "url": "/static/js/runtime-main.460bb9b7.js"
+  }
+]);
